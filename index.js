06b520e7e2028106cf44145d58b52e04f93576cc
Charli Wild <charlie.wild@thinkmoney.co.uk>
2021-06-08 12:54:48 +0100

    first go

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
+let accountBalance = 0;
+let transactionHistory = [];
+
+
 function sum(a, b) {
     return a + b;
 }
+
+function deposit(amount) {
+    accountBalance += amount;
+    transactionHistory.push(new Date().toLocaleDateString('en-GB') + " || " + amount + " || " + accountBalance);
+
+}
+
+function withdraw(amount) {
+    accountBalance -= amount;
+    transactionHistory.push(new Date().toLocaleDateString('en-GB') + " || -" + amount + " || " + accountBalance);
+
+}
+
+function printStatement() {
+    console.log("Date || Amount || Balance")
+    for (let i = 0; i < transactionHistory.length; i++) {
+        console.log(transactionHistory[i]);
+    }
+}
+
+function queryBalance() {
+    return accountBalance;
+}
+
+function resetBalance() {
+    accountBalance = 0;
+}
+
+
+
+
 module.exports = {
-    sum,
+    sum, deposit, withdraw, printStatement, queryBalance, resetBalance
 };
--- index.test.js
+++ index.test.js
@@ -1,5 +1,21 @@
-const { sum } = require('./index.js');
+const { sum, deposit, withdraw, queryBalance, resetBalance } = require('./index.js');
+
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+test('when 50 is deposited, the total balance is 50', () => {
+    deposit(50);
+    expect(queryBalance()).toBe(50);
+    resetBalance();
+
+})
+
+test('when 50 is deposited, then 20 is withdraw, the totalBalance is 30', () => {
+    deposit(50);
+    withdraw(20);
+    expect(queryBalance()).toBe(30);
+    resetBalance();
+})
+
